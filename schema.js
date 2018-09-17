673b30b63dff3d2446738762e97e43acfe905144
rhmtrz <2249301106@qq.com>
2018-09-17 11:27:21 +0900

    feat: json-server axios, with new fields

--- schema.js
+++ schema.js
@@ -3,17 +3,21 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
-  GraphQlList,
+  GraphQLList,
   GraphQLNonNull,
 } = require('graphql');
 
-
+const axios = require('axios');
+/*
 //hardcoded data
 const customers = [
   {id: '1', name: 'Rahmat', email: 'rezaei@adawarp.com', age: 27 },
   {id: '2', name: 'Adaniya', email: 'adaniya@adawarp.com', age: 25 },
   {id: '3', name: 'Zigen', email: 'zigen@adawarp.com', age: 25 },
 ]
+*/
+
+
 
 // Customer type
 const CustomerType = new GraphQLObjectType({
@@ -36,16 +40,27 @@ const RootQuery = new GraphQLObjectType({
       args: {
         id: {type: GraphQLString}
       },
+      resolve(parentValue, args){
+      //   for (let i = 0; i < customers.length; i++) {
+      //     if (customers[i].id == args.id) {
+      //       return customers[i];
+      //     }
+      //   }
+
+        return axios.get('http://localhost:3000/customers/' + args.id)
+          .then(res => res.data);
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
-          if (customers[i].id == args.id) {
-            return customers[i];
-          }
-        }
+        return axios.get('http://localhost:3000/customers')
+          .then(res => res.data);
       }
     }
   }
-})
+});
+
 
 module.exports = new GraphQLSchema({
   query: RootQuery
